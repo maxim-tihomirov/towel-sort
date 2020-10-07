0d74ececffeb4b4136d8ad3557aed5aa437910bc
maxim-tihomirov <maxbeere@yandex.ru>
2020-10-07 11:17:42 +0300

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  return !matrix ? [] : matrix.map((el,index) => {
+  	if (index % 2 != 0){
+    	el.reverse();
+    }
+    return el;
+  }).flat(Infinity);
 }
